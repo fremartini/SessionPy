2762d3509b8ef4c66eddd6a600a4ad330b10d850
Johan Irvall Rasmussen <johanirvall@gmail.com>
2021-11-22 13:52:56 +0100

    examples updated

--- examples/bad4.py
+++ examples/bad4.py
@@ -3,16 +3,18 @@ from sessiontype import *
 from fromfile import check_file
 
 def f(c: Channel):
-    c.send(666)
-    print('sent value', 666) # should NOT happen; at this point we receive
-    return True
+    b = c.recv()
+    print('received value', b) # should happen; at this point we receive
+    return b
 
 
 def main():
-    ch = Channel[Send[int, Recv[bool, ]]]()
+    ch = Channel[Send[int, Recv[bool, Send[str, End]]]]()
     ch.send(42) 
     print('sent value', True)  # okay so far
     f(ch)
+    s = ch.recv()
+    print('received', s) # should NOT happen; at this point we should send
 
 if __name__ == '__main__':
     main()
