e3a8141e688ddd6a8eccd63b7bb101316596ad96
fremartini <frederik.martini@live.dk>
2021-11-22 12:14:09 +0100

    hacked python

--- sessiontype.py
+++ sessiontype.py
@@ -12,5 +12,21 @@ class Recv(Generic[A, ST]):
 
 class End:
     ...
-c : Channel[Send[int, Recv[bool, End]]] = Channel()
-#Channel[Send[int, Recv[bool, End]]].__args__ => (__main__.Send[int, __main__.Recv[bool, __main__.End]],)
+
+# Exctracting type info
+c = Channel[Recv[str, Send[int, End]]]()
+#typ = c.__orig_class__.__args__[0] # extract channels args where type info lies
+#typ.__origin__ # returns currenct session type action i.e. Send, Recv or End
+#typ.__args__ # this "steps into" our session type: returns tuple with (Int, {rest}) but removes outer action, i.e. Send
+
+def extract(c):
+    return builder(c.__orig_class__.__args__[0]) 
+
+def builder(st):
+    if st == End:
+        return []
+    action = st.__origin__
+    typ, rest = st.__args__ 
+    return [(action, typ)] + builder(rest)
+    
+print(extract(c))
