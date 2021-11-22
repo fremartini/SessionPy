2e517ff49fa13719d5ac52010aa28d036b2d478d
fremartini <frederik.martini@live.dk>
2021-11-22 12:27:53 +0100

    restructure sessiontype

--- sessiontype.py
+++ sessiontype.py
@@ -1,32 +1,36 @@
-from channel import *
+from channel import Channel
 from typing import TypeVar, Generic
-from inspect import *
 
 ST = TypeVar('ST')
 A = TypeVar('A')
-class Send(Generic[A, ST]):
+
+class SessionType:
+    ...
+
+class Send(SessionType, Generic[A, ST]):
     ...
     
-class Recv(Generic[A, ST]):
+class Recv(SessionType, Generic[A, ST]):
     ...
 
-class End:
+class End(SessionType):
     ...
 
-# Exctracting type info
-c = Channel[Recv[str, Send[int, End]]]()
-#typ = c.__orig_class__.__args__[0] # extract channels args where type info lies
-#typ.__origin__ # returns currenct session type action i.e. Send, Recv or End
-#typ.__args__ # this "steps into" our session type: returns tuple with (Int, {rest}) but removes outer action, i.e. Send
+def is_session_type(t : type):
+    return issubclass(t, SessionType)
 
-def extract(c):
+def extract(c : Channel):
     return builder(c.__orig_class__.__args__[0]) 
 
-def builder(st):
+def builder(st : SessionType):
     if st == End:
         return []
     action = st.__origin__
     typ, rest = st.__args__ 
     return [(action, typ)] + builder(rest)
-    
-print(extract(c))
+
+# Exctracting type info
+#c = Channel[Recv[str, Send[int, End]]]()
+#typ = c.__orig_class__.__args__[0] # extract channels args where type info lies                                         => __main__.Recv[str, __main__.Send[int, __main__.End]]
+#typ.__origin__ # returns currenct session type action i.e. Send, Recv or End                                            => <class '__main__.Recv'>
+#typ.__args__ # this "steps into" our session type: returns tuple with (Int, {rest}) but removes outer action, i.e. Send => (<class 'str'>, __main__.Send[int, __main__.End])
