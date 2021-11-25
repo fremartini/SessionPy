c4c4fbbeeb65d4a49de5fbaa3cf14cf6c3c31de5
fremartini <frederik.martini@live.dk>
2021-11-25 22:36:47 +0100

    handle recv in check_channel_usage

--- ast_playground.py
+++ ast_playground.py
@@ -35,53 +35,46 @@ class Analyzer(ast.NodeVisitor):
         for stmt in nd:
             match stmt:
                 case ast.Assign(): self.check_assign(stmt)
-                case ast.Call(): self.check_call(stmt)
-
-    def check_call(self, stm):
-        dump('check_call', stm)
                 
     def check_assign(self, asgn):
         # x = y
         # 1: ch = Channel...
         # 2: <var> = ch.send/recv
         t, v = *asgn.targets, asgn.value # TODO: only allowing 1:1 assignment mapping (not i.e. ch1, ch2 = Channel(), Channel())
-        dump('value', v)
         if(isinstance(v, ast.Call)):
             self.check_channel_usage(t, v)
-        # print('assign', t.id, 'to')
-        # print(v.id)
         match v:
             case ast.Call(): self.search_call(t, v)
 
     def check_channel_usage(self, target, value):
-        dump('check_channel_usage target', target)
         assert(isinstance(target, ast.Name))
         assert(isinstance(value, ast.Call))
-        # if target.id in self.channels: # 
-        dump("call", value)
         attri = value.func
-        if(isinstance(attri, ast.Attribute)): 
-            dump('arg', value.args[0])
-            channel_name = attri.value.id # channel variable (i.e. c, ch, etc)
+        if(isinstance(attri, ast.Attribute)):       # channel usage : v = ch.recv()
+            channel_name = attri.value.id           
             op = attri.attr
-            if op == 'send':
-                assert(len(value.args) == 1)
-                value = value.args[0]
-                if isinstance(value, ast.Constant):
-                    value = value.value
-                st = self.channels[channel_name]
-                valTyp = str(type(value))
-                (action,typ), *tail = st
-                self.channels[channel_name] = tail
-                # assert(valTyp == typ) #'int' != int
+            match op:
+                case 'send':                        # only works if send returns something e.g : v = ch.send(10)
+                    assert(len(value.args) == 1)
+                    value = value.args[0]
+                    if isinstance(value, ast.Constant):
+                        value = value.value
+                    st = self.channels[channel_name]
+                    valTyp = type(value)
+                    (action,typ), *tail = st
+                    assert(valTyp == typ)
+                    assert(action == op)
+                    self.channels[channel_name] = tail
+                case 'recv':
+                    assert(len(value.args) == 0)
+                    st = self.channels[channel_name]
+                    (action,typ), *tail = st        # TODO: ch = Channel[Recv[int, End]]() crashes, not enough values to unpack
+                    assert(action == op)
+                    self.channels[channel_name] = tail
+                    print(self.channels[channel_name])
+        elif(isinstance(attri, ast.Subscript)):     # channel definition : ch = Channel[Send[int, End]]()
+            ...
                 
-    def mapStrToTyp(s):
-        match s:
-            case 'int': return int
-            case 'str': return str
-            case 'bool': return bool
-            case _: raise Exception (f"unknown type {s}")
-
     def search_call(self, target, call_obj):
         f = call_obj.func
         if isinstance(f, ast.Subscript) and f.value.id == 'Channel': 
@@ -100,27 +93,29 @@ class Analyzer(ast.NodeVisitor):
     def search_slice(self, sliced):
         acc = []
         if isinstance(sliced, ast.Tuple): 
-            dump('tuple', sliced)
-            print('tup', [x.id for x in sliced.dims])
             # A tuple of the form (<type>, Subscript) where Subcscript should be recursively called
             for dim in sliced.dims:
                 if isinstance(dim, ast.Name):
                     if dim.id != 'End':
-                        acc.append(dim.id)
+                        acc.append(self.strToTyp(dim.id))
                 else:
                     assert(isinstance(dim, ast.Subscript))
                     tmp = self.search_slice(dim)
                     acc += tmp
-            #for elt in slice.elts: # TODO: this contains the same – investigate which one to keep
-            #    print('elt', elt.id)
         if isinstance(sliced, ast.Subscript):
             name = sliced.value # This is the ast.Name of the action, i.e. Send/Recv/End
             assert(isinstance(name, ast.Name))
             tmp = self.search_slice(sliced.slice)
-            acc.append(name.id)
+            acc.append(str.lower(name.id))
             acc += tmp
         return acc
 
+    def strToTyp(self, s):
+        match s:
+            case 'int': return int
+            case 'str': return str
+            case 'bool': return bool
+            case _: raise Exception (f"unknown type {s}")
 
     def report(self):
         pprint(self.channels)
