c095cf7ef80156b62f98c81b7bfaa87ab5ab3882
Johan Irvall Rasmussen <johanirvall@gmail.com>
2021-12-18 11:59:48 +0100

    fixed program; still buggy

--- example_branchB.py
+++ example_branchB.py
@@ -5,16 +5,14 @@ from typechecking import verify_channels
 @verify_channels
 def main():
     #my_channel = Channel[Recv[int, Choose[Send[str, End], Recv[int, End]]]]()
-    my_channel = Channel[Recv[int, Choose[Send[str, End], Choose[Send[int, Recv[int, End]]]]]]()
+    my_channel = Channel[Recv[int, Choose[Send[str, End], Choose[Send[int, End], Recv[int, End]]]]]()
     n = my_channel.recv()
 
     if n > 5:
         my_channel.choose(Branch.LEFT)
-        print("B: left: sending string")
         my_channel.send("number was greater than 5")
     else:
         my_channel.choose(Branch.RIGHT)
-        print("B: right: receiving integer")
         if n < 12:
             my_channel.choose(Branch.RIGHT)
             i = my_channel.recv()
