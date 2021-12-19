ef182cb87e1fd36cc60a36e667310101b22eb53c
fremartini <frederik.martini@live.dk>
2021-12-19 13:52:30 +0100

    Committing in accordance with the prophecy.

--- channel_tests.py
+++ channel_tests.py
@@ -125,5 +125,39 @@ class TestVerifyChannels(unittest.TestCase):
                 print('received', s) # should NOT happen; at this point we should send
     """
 
+    def test_bad5(self):
+        with self.assertRaises(Exception):
+            @verify_channels
+            def main():
+                ch = Channel[Send[int, Offer[Send[str, Recv[int, End]], Send[int, End]]]]()
+                ch.send(5)
+
+                match ch.offer():
+                    case Branch.LEFT:
+                        print("A: receiving message from client (B)")
+                        ch.send("hello!")
+                        
+                    case Branch.RIGHT:
+                        print("A: sending number to client (B)")
+                        ch.send(42)
+
+    def test_bad6(self):
+        with self.assertRaises(Exception):
+            @verify_channels
+            def main():
+                ch = Channel[Recv[int, Choose[Send[str, End], Choose[Send[int, End], Recv[int, End]]]]]()
+                n = ch.recv()
+
+                if 10 > 5:
+                    ch.choose(Branch.LEFT)
+                    ch.send("number was greater than 5")
+                else:
+                    ch.choose(Branch.RIGHT)
+                    if 1 + 3 > 4:
+                        ch.choose(Branch.LEFT)
+                        ch.send(1)
+                    else:
+                        ch.choose(Branch.RIGHT)
+
 if __name__ == '__main__':
     unittest.main()
