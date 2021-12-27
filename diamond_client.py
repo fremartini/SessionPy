8095e44f8fd76567686484f03118e8a353114a4b
Johan Irvall Rasmussen <johanirvall@gmail.com>
2021-12-27 13:24:44 +0100

    cleanup example

--- diamond_client.py
+++ diamond_client.py
@@ -3,21 +3,24 @@ from sessiontype import *
 from typechecking import verify_channels
 from enum import Enum
 
-class DiamondColor(Enum):
+
+class DiamondColour(Enum):
     RED = 0
     BLUE = 1
     YELLOW = 2
 
+
 @verify_channels
 def main():
-    my_balance = 50_000 # USD
-    ch = Channel[Send[str, Recv[dict[DiamondColor, int], Choose[Recv[str, End]], Send[DiamondColor, Recv[str, End]]]]]()
+    my_balance = 50_000  # USD
+    ch = Channel[Send[str, Recv[dict[DiamondColour, int],
+                                Choose[Recv[str, End], Send[DiamondColour, Recv[str, End]]]]]]()
     ch.send("Hi! I'd like to purchase a diamond")
     catalogue = ch.recv()
     best_diamond = None
-    for color, price in catalogue:
-        if price <= my_balance:
-            best_diamond = color
+    for colour, price in catalogue:
+        if price <= my_balance and price > catalogue[best_diamond]:
+            best_diamond = colour
     if not best_diamond:
         ch.choose(Branch.LEFT)
         response = ch.recv()
@@ -26,9 +29,7 @@ def main():
         ch.choose(Branch.RIGHT)
         ch.send(best_diamond)
         receipt = ch.recv()
-        print('Response from seller:', receipt)
-
-
+        print('Receipt from seller:', receipt)
 
 
 if __name__ == '__main__':
--- diamond_seller.py
+++ diamond_seller.py
@@ -3,32 +3,32 @@ from sessiontype import *
 from typechecking import verify_channels
 from enum import Enum
 
+
 class DiamondColour(Enum):
     RED = 0
     BLUE = 1
     YELLOW = 2
 
+
 @verify_channels
 def main():
-    my_balance = 50_000 # USD
-    ch = Channel[Recv[str, Send[dict[DiamondColour, int], Offer[Recv[str, Send[str, End]], Recv[DiamondColour, Send[str, End]]]]]]()
+    ch = Channel[Recv[str, Send[dict[DiamondColour, int],
+                                Offer[Recv[str, Send[str, End]], Recv[DiamondColour, Send[str, End]]]]]]()
     req = ch.recv()
+    print('Received request from seller:', req)
     catalogue = {
-        DiamondColour.RED : 1_000_000,
-        DiamondColour.BLUE : 500_000,
+        DiamondColour.RED: 1_000_000,
+        DiamondColour.BLUE: 500_000,
         DiamondColour.YELLOW: 50_000,
     }
     ch.send(catalogue)
     match ch.offer():
         case Branch.LEFT:
-            decline_msg = ch.recv()
             ch.send("It is okay, my diamonds are very expensive")
         case Branch.RIGHT:
             colour = ch.recv()
-            ch.send(f'Thanks for purchasing a {colour} diamond, you spent ${catalogue[colour]}')
-  
-
-
+            ch.send(
+                f'Thanks for purchasing a {colour} diamond, you spent ${catalogue[colour]}')
 
 
 if __name__ == '__main__':
