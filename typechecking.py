83b3ff21d1ff3d91a657a0984d96bd35204216f5
Frederik Martini <fma@redbarnet.dk>
2021-12-30 13:05:44 +0100

    docs

--- typechecking.py
+++ typechecking.py
@@ -14,7 +14,7 @@ def verify_channels(f):
 
 
 def scan(func):
-    """Scan the file the decorated function belongs to, for functions that has Channels as parameters"""
+    """Scan the decorated function, for channel assignments and functions that has Channels as parameters"""
     func_src = dedent(inspect.getsource(func))
     func_ast = ast.parse(func_src)
     functions, channels = Scanner(func_ast).run()
@@ -23,12 +23,7 @@ def scan(func):
 
 
 def check(func, functions, channels):
-    """Check that Channels are used correctly in all functions which they are used"""
+    """Check that Channels are used correctly in all functions in which they are used"""
     src = dedent(inspect.getsource(func))
     tree = ast.parse(src)
     Checker(tree, functions, channels).run()
-
-
-def _read_src_from_file(file):
-    with open(file, "r") as f:
-        return f.read()
