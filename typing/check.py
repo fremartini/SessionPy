f1b26e9ac0ca624b3a0589b7c441c64d0d2ccf3b
fremartini <frederik.martini@live.dk>
2022-02-09 17:13:14 +0100

    union

--- typing/check.py
+++ typing/check.py
@@ -2,6 +2,7 @@ import inspect
 from ast import *
 from typing import *
 from textwrap import dedent
+import types
 
 def check(f : callable) -> callable:
     file = dedent(inspect.getfile(f))         
@@ -17,12 +18,24 @@ def _read_src_from_file(file) -> Str:
 def dump_ast(s, node) -> None:
     print(f'{s}\n', dump(node, indent=4))
 
-def strToTyp(s):
+def str_to_typ(s : str) -> type:
     match s:
         case 'int': return int
         case 'str': return str
+        case 'float': return float
         case 'bool': return bool
         case _: raise Exception(f"unknown type {s}")
+
+class UnionError(Exception):
+    ...
+
+def union(t1: type, t2: type) -> type:
+    if t1 == t2: return t1
+    match t1, t2:
+        case (typ1, typ2) if typ1 is int and typ2 is int: return int
+        case (typ1, typ2) if typ1 is int and typ2 is float: return float
+        case (typ1, typ2) if typ1 is float and typ2 is int: return float
+        case (typ1, typ2): raise UnionError(f"Cannot union {typ1} with {typ2}")
 
 class TypeChecker(NodeVisitor):
     def __init__(self, tree) -> None:
@@ -31,7 +44,6 @@ class TypeChecker(NodeVisitor):
 
     def visit_Module(self, node: Module) -> None:
         for stmt in node.body:
-            dump_ast("#STMT", stmt)
             print(self.environments)
             self.visit(stmt)
 
@@ -51,7 +63,7 @@ class TypeChecker(NodeVisitor):
         match node:
             case node if node.annotation:
                 ann : str = self.visit(node.annotation)
-                ann_typ : type = strToTyp(ann)
+                ann_typ : type = str_to_typ(ann)
                 self.bind(node.arg, ann_typ)
             case _:
                 self.bind(node.arg, Any)
@@ -67,15 +79,21 @@ class TypeChecker(NodeVisitor):
         value : type = self.visit(node.value)
         self.bind(target, value)
 
-
     def visit_AnnAssign(self, node: AnnAssign) -> None:
         target : str = self.visit(node.target)
-        ann : Type = strToTyp(self.visit(node.annotation))
+        ann : Type = str_to_typ(self.visit(node.annotation))
         #FIXME: consider annotated vs inferred type?
         #value : Type = self.visit(node.value)
         #assert ann == value
 
         self.bind(target, ann)
+
+    def visit_BinOp(self, node: BinOp) -> type:
+        l = self.visit(node.left)
+        r = self.visit(node.right)
+        l_typ = self.lookup(l)
+        r_typ = self.lookup(r)
+        return union(l_typ, r_typ)
 
     def visit_Constant(self, node: Constant) -> Type:
         return type(node.value)
@@ -89,6 +107,18 @@ class TypeChecker(NodeVisitor):
     def pop(self) -> None:
         self.environments.pop()
 
+    def lookup(self, key) -> Type:
+        latest_scope : Dict[str, type] = self.get_latest_scope()
+
+        print(f'trying to find {key} in {latest_scope}')
+        if (key in latest_scope):
+            return latest_scope[key]  
+        else:
+            raise Exception(f'{key} was not found in {latest_scope}')  
+        
+    def get_latest_scope(self) -> Dict[str, type]:
+        return self.environments[len(self.environments)-1]
+
     def bind(self, var, val) -> None:
-        latest_scope : Dict[str, type] = self.environments[len(self.environments)-1]
+        latest_scope : Dict[str, type] = self.get_latest_scope()
         latest_scope[var] = val
--- typing/main.py
+++ typing/main.py
@@ -1,13 +1,9 @@
 from check import check
 
 @check #hook into the pre-run phase. Can do better?
-def add1(x):
-    y = 1
-
-class A:
-    ...
-
-a = A()
-k : int = 5
-#should fail
-add1(a)
+def test_binop():
+    x : int = 41        # []
+    y : float = 1.0     # [x -> int]
+    z : int = 100       # [x -> int, y -> float]
+    w = (x + y) + z     # [x -> int, y -> float, z -> int]
+                        # [x -> int, y -> float, z -> int, w -> float]
