1851311c17d7d800d0f999259f38f2ad587a4f80
fremartini <frederik.martini@live.dk>
2022-02-18 11:43:38 +0100

    seperated session-type and typecheck tests

---
+++ tests/tests_sessiontypes/context.py
@@ -0,0 +1,7 @@
+import os
+import unittest
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
+
+from session.channel import *
+from session.typechecking import *
