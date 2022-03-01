33da61d8cb0b7826d15a6e29e533b7778c1a21a2
Johan Irvall Rasmussen <johanirvall@gmail.com>
2022-03-01 12:37:32 +0100

    added debug

---
+++ typecheck/check_debug.py
@@ -0,0 +1,21 @@
+from ast import dump
+
+def dump_ast(s, node) -> None:
+    print(f'{s}\n', dump(node, indent=4))
+
+"""
+    prints object and its type
+    examples:
+        is_a("hello")   => 'hello is a str'
+        is_a([1,2,3])   => '[1,2,3] is a list'
+        is_a(str)       => 'str is a type'
+"""
+def is_a(obj):
+    print(obj, 'is a', type(obj))
+
+
+def fail_if(e: bool, msg: str) -> None:
+    if e:
+        raise Exception(msg)
+
+
