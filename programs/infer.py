7203aebf5e03cabe4328928842ff06288474420b
Johan Irvall Rasmussen <johanirvall@gmail.com>
2022-03-22 11:23:21 +0100

    progress on while

---
+++ programs/infer.py
@@ -0,0 +1,5 @@
+
+def f(x):
+    return x
+
+f(42)
--- programs/loop.py
+++ programs/loop.py
@@ -1,6 +1,15 @@
 from channel import *
 ch = Channel[Label['main', Choose [Send[int, Recv[str, 'main'] ], Recv[bool, Send[float, 'main' ]]]]]()
 
+# # -- TODO: Should this "exhaust" our session type?
+# while True:
+#     print("hello")
+#     ch.choose(Branch.RIGHT)
+#     b = ch.recv()
+#     ch.send(2.123)
+# # -- So that we are "done" here?
+
+
 while True:
     if 2 < 15:
         ch.choose(Branch.RIGHT)
