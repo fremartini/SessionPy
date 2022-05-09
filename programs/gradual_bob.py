98a39b79d4963e05d15f52546f5df0218e6effcf
Johan Irvall Rasmussen <johanirvall@gmail.com>
2022-05-09 13:28:05 +0200

    changes suggested by wiseman Martini

---
+++ programs/gradual_bob.py
@@ -0,0 +1,28 @@
+from sessiontype import *
+from channel import Channel
+
+routing = {'self': ('localhost', 50_000), 'Alice': ('localhost', 50_505)}
+
+Neg = Recv[int, 'Alice', Send[int, 'Alice', End]]
+Add = Recv[int, 'Alice', Recv[int, 'Alice', Send[int, 'Alice', End]]]
+ch = Channel(Offer ['Alice', {"neg": Neg, "add": Add}], routing, static_check = False)
+
+
+negate = lambda x: -x
+add = lambda x: lambda y: x + y
+
+def dynServer(c):
+    match c.offer():
+        case "neg": serveOp(1, negate, c)
+        case "add": serveOp(2, add, c)
+
+def serveOp(n, op, c):
+    if n == 0:
+        c.send(op)
+    else:
+        v = c.recv()
+        serveOp(n-1, op(v), c)
+
+dynServer(ch)
+
+
