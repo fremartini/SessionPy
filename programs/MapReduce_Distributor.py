450deafad6e2fbae8a3aa89d19f57820ed888c0d
fremartini <frederik.martini@live.dk>
2022-05-16 10:48:53 +0200

    correct mistake in MapReduce example

--- programs/MapReduce_Distributor.py
+++ programs/MapReduce_Distributor.py
@@ -1,16 +1,18 @@
 from context import *
 
-roles = {'self': ('localhost', 5000), 'c1': ('localhost', 5001), 'c2': ('localhost', 5002), 'c3': ('localhost', 5003), }
+routing_table = {'self': ('localhost', 5000), 'c1': ('localhost', 5001), 'c2': ('localhost', 5002), 'c3': ('localhost', 5003),}
 
-ch = Channel(
-    Send[List[int], 'c1', Send[List[int], 'c2', Send[List[int], 'c3', Recv[int, 'c1', Recv[int, 'c2', Recv[int, 'c2', End]]]]]], roles)
+ch = Channel(Send[List[int], 'c1', Send[List[int], 'c2', Send[List[int], 'c3', Recv[int, 'c1', Recv[int, 'c2', Recv[int, 'c3', End]]]]]], routing_table)
 
 ch.send([1, 2, 3])
 ch.send([4, 5, 6])
 ch.send([7, 8, 9])
 
 r1 = ch.recv()
+print(r1)
 r2 = ch.recv()
+print(r2)
 r3 = ch.recv()
+print(r3)
 
 print(r1 + r2 + r3)
--- programs/MapReduce_c3.py
+++ programs/MapReduce_c3.py
@@ -3,12 +3,16 @@ import time
 
 from context import *
 
-roles = {'Distributor': ('localhost', 5000), 'c1': ('localhost', 5001), 'c2': ('localhost', 5002), 'self': ('localhost', 5003),}
+routing_table = {'Distributor': ('localhost', 5000), 'c1': ('localhost', 5001), 'c2': ('localhost', 5002), 'self': ('localhost', 5003),}
 
-ch = Channel(Recv[List, 'Distributor', End], roles, static_check=False)
+ch = Channel(Recv[List[int], 'Distributor', Send[int, 'Distributor', End]], routing_table)
 
 workload = ch.recv()
 
+print(workload)
+
 time.sleep(random.randint(3, 9))
+
+print('sending')
 
 ch.send(random.randint(50, 100))
