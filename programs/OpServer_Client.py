4e288c4095e89a0d134d1a0dfd52e01941fbaf0c
fremartini <frederik.martini@live.dk>
2022-05-23 16:30:14 +0200

    opserver

--- programs/OpServer_Client.py
+++ programs/OpServer_Client.py
@@ -1,5 +1,10 @@
 from context import *
 
-routing_table = {'Server': ('localhost', 0), 'self': ('localhost', 0)}
+routing_table = {'Server': ('localhost', 5000), 'self': ('localhost', 5001)}
 
 ch = Channel(Choose['Server', {"add": Send[int, 'Server', Send[int, 'Server', Recv[int, 'Server', End]]], "neg": Send[int, 'Server', Recv[int, 'Server', End]], "mul": Send[int, 'Server', Send[int, 'Server', Recv[int, 'Server', End]]]}], routing_table)
+
+ch.choose('mul')
+ch.send(5)
+ch.send(7)
+print(ch.recv())
